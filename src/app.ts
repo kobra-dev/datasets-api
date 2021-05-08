2feee9c85d17df91575a0725caced9c54fbb1715
veritem <mugaboverite@gmail.com>
2021-05-09 00:26:33 +0200

    feat: initital commit

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import { FastifyPluginAsync } from 'fastify'
+import AutoLoad, { AutoloadPluginOptions } from 'fastify-autoload'
+import { join } from 'path'
+
+export type AppOptions = {} & Partial<AutoloadPluginOptions>
+
+const app: FastifyPluginAsync<AppOptions> = async (
+    fastify,
+    opts,
+): Promise<void> => {
+    void fastify.register(AutoLoad, {
+        dir: join(__dirname, 'plugins'),
+        options: opts,
+    })
+
+    void fastify.register(AutoLoad, {
+        dir: join(__dirname, 'routes'),
+        options: opts,
+    })
+}
+
+export default app
+export { app }
