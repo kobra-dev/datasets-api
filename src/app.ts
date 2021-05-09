bbcda077f485576acdb808e08df9641fd2ee1df9
veritem <mugaboverite@gmail.com>
2021-05-09 23:07:46 +0200

    feat: done

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,15 @@
 import { FastifyPluginAsync } from 'fastify'
 import AutoLoad, { AutoloadPluginOptions } from 'fastify-autoload'
-// import fastifyEnv from 'fastify-env'
+import fastifyMultipart from 'fastify-multipart'
 import { join } from 'path'
 
 export type AppOptions = {} & Partial<AutoloadPluginOptions>
-
-// const schema = {
-//     type: 'object',
-//     require: ['PORT'],
-//     properties: {
-//         PPORT: {
-//             type: 'string',
-//             default: '3000',
-//         },
-//     },
-// }
 
 const app: FastifyPluginAsync<AppOptions> = async (
     fastify,
     opts,
 ): Promise<void> => {
-    // void fastify.register(fastifyEnv, {
-    //     schema,
-    // })
+    void fastify.register(fastifyMultipart, { attachFieldsToBody: true })
 
     void fastify.register(AutoLoad, {
         dir: join(__dirname, 'plugins'),
--- src/routes/dataset.ts
+++ src/routes/dataset.ts
@@ -1,19 +1,76 @@
+import S3 from 'aws-sdk/clients/s3'
 import { FastifyPluginAsync } from 'fastify'
+import http from 'http'
+import { v4 as uuid } from 'uuid'
+import isURL from 'validator/lib/isURL'
 
-const example: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-    fastify.post('/', async function (request, reply) {
-        return 'this is an example'
+const datasets: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
+    const s3 = new S3({
+        endpoint: process.env.API_END_POINT,
+        accessKeyId: process.env.ACCESS_KEY,
+        secretAccessKey: process.env.SECRET_KEY,
     })
 
-    fastify.post('/:url', async function (request, reply) {
-        return 'read from the url'
+    fastify.post('/dataset', async function (request: any, reply) {
+        const encodedDataset = await request.body.upload
+            .toBuffer()
+            .toString('base64')
+
+        const params = {
+            Body: encodedDataset,
+            Bucket: 'kobra',
+            Key: uuid(),
+        }
+
+        s3.putObject(params, function (err: any, data: any) {
+            if (err) throw new Error(err)
+        })
+
+        reply.status(201).send({
+            message: 'file uploaded successfully',
+            key: params.Key,
+        })
     })
 
-    fastify.get('/:id', async function (request, reply) {
-        console.log(request.params)
+    fastify.get('/dataset/:key', async function (request: any, reply) {
+        const params = {
+            Bucket: 'kobra',
+            Key: request.params.key,
+        }
 
-        return 'Hello world'
+        //TODO: fix loading the buffer
+        s3.getObject(params, async function (err, data) {
+            if (err) reply.badRequest(err.message)
+
+            let dataset: any = data.Body
+
+            try {
+                dataset = await dataset.toString('utf-8')
+                reply.send({ dataset })
+            } catch (error) {}
+        })
+
+        reply.status(200).send({ success: true, params })
+    })
+
+    fastify.post('/dataset/url', async function (request: any, reply) {
+        const { url } = request.body
+
+        if (!isURL(url))
+            reply
+                .status(404)
+                .send({ sucess: true, message: 'Invalid url passed' })
+
+        //TODO: fix file downloading
+        const file = http.get(url, function (resp) {
+            console.log(resp)
+        })
+        console.log({ file })
+
+        reply.status(200).send({
+            success: true,
+        })
     })
 }
 
-export default example
+export default datasets
