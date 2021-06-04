c27fb508d3a541a78057a7b84b6472c05d18ede5
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-06-04 14:12:41 +0200

    (fix): remote legacy

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,17 +1,5 @@
 import crypto from 'crypto'
 
-interface FileType {
-    name: string
-    size: string
-    type: string
-    path: string
-    file: Buffer
-    extension: string
-    content: ArrayBuffer
-}
-
 export function hashString(name: string) {
     return crypto.createHash('sha256').update(name).digest('hex')
 }
-
-export { FileType }
